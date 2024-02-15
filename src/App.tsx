957883e34f3d91d23998de41c25ec37431fc3b65
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-02-14 21:37:57 -0300

    chore: make small changes in css content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import '@/styles/global.css';
 
 export function App() {
-	return <h1 className="text-3xl font-bold text-slate-400">Hello, World!</h1>;
+	return <h1 className="text-3xl text-slate-50">Hello, Tailwind!</h1>;
 }
