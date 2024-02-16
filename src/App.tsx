5f2fc48d979ef98b5980f792470781757ec63be9
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-02-15 23:58:07 -0300

    chore: add an underline behavior for the hello world element

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import '@/styles/global.css';
 
 export function App() {
-	return <h1 className="text-3xl text-slate-50">Hello, Tailwind!</h1>;
+	return <h1 className="relative text-3xl text-slate-50 after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-slate-50 hover:after:w-full">Hello, Tailwind!</h1>;
 }
